1d673ba65286ef66bbd8c57c0e22f332feec7584
LiTaohong <1026980510@qq.com>
2018-09-13 14:26:05 +0800

    小工具类

---
+++ src/main/java/com/ongoing/util/BeanUtil.java
@@ -0,0 +1,66 @@
+package com.ongoing.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @project figure-label
+ * @author litaohong on 2018/3/27
+ */
+public class BeanUtil {
+
+    /**
+     * 获取类字段名
+     * @param obj
+     * @return
+     */
+    public static String[] getFiledName(Object obj) {
+        try {
+            Field[] fields = obj.getClass().getDeclaredFields();
+            String[] fieldNames = new String[fields.length];
+            for (int i = 0; i < fields.length; i++) {
+                fieldNames[i] = fields[i].getName();
+            }
+            return fieldNames;
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将对象转换成map
+     * @param obj
+     * @return
+     * @throws IllegalAccessException
+     */
+    public static Map<String, String> beanToMap(Object obj) {
+        Map<String, String> map = new HashMap<>();
+        try {
+            Class<?> clazz = obj.getClass();
+            for (Field field : clazz.getDeclaredFields()) {
+                field.setAccessible(true);
+                String fieldName = field.getName();
+                Object value = field.get(obj);
+                map.put(fieldName, value.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    public static Object mapToBean(Map<String, Object> map, Class<?> beanClass) throws Exception {
+
+        if (map == null){
+            return null;
+        }
+
+        Object bean = beanClass.newInstance();
+        org.apache.commons.beanutils.BeanUtils.populate(bean, map);
+        return bean;
+    }
+}
